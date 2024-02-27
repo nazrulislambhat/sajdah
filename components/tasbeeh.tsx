1d9c97c6771e1ab8b83dd257204ae26d43ce75ef
Nazrul Islam <nazrulislambhat@gmail.com>
2024-02-27 15:49:44 +0530

    Animation

--- components/tasbeeh.tsx
+++ components/tasbeeh.tsx
@@ -102,7 +102,7 @@ export default function TasbeehCounter() {
 
       <button
         onClick={resetCount}
-        className="box-border relative z-30 inline-flex items-center justify-center w-auto px-8 py-3 overflow-hidden font-bold text-LightSalahSync transition-all duration-300 bg-SecondarySalahSync rounded-md cursor-pointer group ring-offset-2 ring-1 ring-PastelRedSalahSync ring-offset-PastelRedSalahSync hover:ring-offset-PastelRedSalahSync ease focus:outline-none"
+        className="box-border relative z-30 inline-flex items-center justify-center w-auto px-8 py-3 overflow-hidden font-semibold text-LightSalahSync transition-all duration-300 bg-SecondarySalahSync rounded-md cursor-pointer group ring-offset-2 ring-1 ring-PastelRedSalahSync ring-offset-PastelRedSalahSync hover:ring-offset-PastelRedSalahSync ease focus:outline-none"
       >
         <span className="absolute bottom-0 right-0 w-8 h-20 -mb-8 -mr-5 transition-all duration-300 ease-out transform rotate-45 translate-x-1 bg-LightSalahSync opacity-10 group-hover:translate-x-0"></span>
         <span className="absolute top-0 left-0 w-20 h-8 -mt-1 -ml-12 transition-all duration-300 ease-out transform -rotate-45 -translate-x-1 bg-LightSalahSync opacity-10 group-hover:translate-x-0"></span>

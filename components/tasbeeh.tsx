37fd496b0c5752e0975dc2a655a36e66b0d7db7f
Nazrul Islam <nazrulislambhat@gmail.com>
2024-02-27 15:15:12 +0530

    Animation

--- components/tasbeeh.tsx
+++ components/tasbeeh.tsx
@@ -2,6 +2,7 @@
 import { useState, useEffect, useCallback } from 'react';
 import Tasbih from '../public/Tasbih.png';
 import Image from 'next/image';
+import { motion } from 'framer-motion'; // Import motion from Framer Motion
 
 export default function TasbeehCounter() {
   const [count, setCount] = useState(0);
@@ -47,17 +48,26 @@ export default function TasbeehCounter() {
   };
 
   return (
-    <div className="min-w-96 w-96 px-48 py-8 flex items-center flex-col justify-center border-2 border-BlackSalahSync rounded">
-      <h2 className="flex items-center flex-col justify-center w-24 min-w-24 text-8xl mb-4 text-BlueSalahSync">
+    <motion.div
+      initial={{ opacity: 0, scale: 0.5 }} // Initial animation when component loads
+      animate={{ opacity: 1, scale: 1 }} // Animation when component is rendered
+      className="min-w-96 w-96 px-48 py-8 flex items-center flex-col justify-center border-2 border-BlueSalahSync rounded"
+    >
+      <motion.h2
+        className="flex items-center flex-col justify-center w-24 min-w-24 text-8xl mb-4 text-BlueSalahSync"
+        animate={{ scale: [1, 1.1, 1] }} // Animation on every render
+      >
         {count}
-      </h2>
-      <button
+      </motion.h2>
+      <motion.button
         className="text-6xl border-2 rounded-full p-12 max-h-10 max-w-10 text-LightSalahSync flex items-center flex-col justify-center hover:border-BlueSalahSync bg-SecondarySalahSync hover:bg-YellowSalahSync hover:text-BlueSalahSync transition duration-300 ease-in-out transform hover:scale-105 mb-4"
         onClick={incrementCount}
+        whileHover={{ scale: 1.1 }} // Animation on hover
+        whileTap={{ scale: 0.9 }} // Animation on tap/click
       >
         +
-      </button>
+      </motion.button>
       <button onClick={resetCount}>Reset</button>
-    </div>
+    </motion.div>
   );
 }

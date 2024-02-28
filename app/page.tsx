081e8d86dbe3e1f73fc8d4b1508c034ae6c9c1d2
Nazrul Islam <nazrulislambhat@gmail.com>
2024-02-28 14:22:42 +0530

    Changes

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,14 @@
 'use client';
 import Footer from '@/components/footer';
+import RamadanModeSwitch from '@/components/ramadanSwitch';
 import TasbeehCounter from '@/components/tasbeeh';
 import Image from 'next/image';
 import Header from '@/components/header';
 
 export default function Home() {
   return (
-    <main className="min-h-screen boxed bg-LightSalahSync">
+    <main className="min-h-screen boxed bg-LightSalahSync flex flex-col items-center">
+      <RamadanModeSwitch />
       <TasbeehCounter />
     </main>
   );
---
+++ components/ramadanSwitch.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import { Switch } from '@nextui-org/react';
+
+export default function RamadanSwitch() {
+  const [isSelected, setIsSelected] = React.useState(true);
+
+  return (
+    <div className="flex flex-col items-center gap-2">
+      <h2 className="text-PrimarySalahSync text-xl font-semibold">
+        Ramadan mode
+      </h2>
+      <Switch
+        isSelected={isSelected}
+        onValueChange={setIsSelected}
+        color="success"
+      ></Switch>
+    </div>
+  );
+}

6811a390bd3324b0c57fc2e7835d38c57c9d17d8
Nazrul Islam <nazrulislambhat@gmail.com>
2024-02-29 15:27:19 +0530

    HadithCards

--- app/page.tsx
+++ app/page.tsx
@@ -4,13 +4,12 @@ import RamadanModeSwitch from '@/components/ramadanSwitch';
 import TasbeehCounter from '@/components/tasbeeh';
 import Image from 'next/image';
 import Header from '@/components/header';
-import HadithCards from '@/components/hadithCards';
+
 export default function Home() {
   return (
     <main className="min-h-screen boxed bg-LightSalahSync flex flex-col gap-4 xl:gap-8 items-center ">
       <RamadanModeSwitch />
       <TasbeehCounter />
-      <HadithCards />
     </main>
   );
 }

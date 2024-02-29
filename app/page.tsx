80919830c0c66e9713373eb0902a603cc54f8d5c
Nazrul Islam <nazrulislambhat@gmail.com>
2024-02-29 19:13:33 +0530

    HadithCards

--- app/page.tsx
+++ app/page.tsx
@@ -4,10 +4,11 @@ import Image from 'next/image';
 import Header from '@/components/header';
 import Link from 'next/link';
 import Hero from '@/components/hero';
+import { MacbookScroll } from '@/components/ui/macbook';
 export default function Home() {
   return (
     <main className="min-h-screen boxed bg-LightSalahSync flex flex-col gap-4 xl:gap-8 items-center ">
-     
+      <MacbookScroll />
     </main>
   );
 }
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,13 +1,48 @@
+// import type { Config } from 'tailwindcss';
+// const { nextui } = require('@nextui-org/react');
+
+// const config: Config = {
+//   content: [
+//     './pages/**/*.{js,ts,jsx,tsx,mdx}',
+//     './components/**/*.{js,ts,jsx,tsx,mdx}',
+//     './app/**/*.{js,ts,jsx,tsx,mdx}',
+//     './node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}',
+//   ],
+//   theme: {
+//     extend: {
+//       colors: {
+//         PrimarySalahSync: '#3423A6',
+//         SecondarySalahSync: '#8CD867',
+//         LightSalahSync: '#EBF2FA',
+//         WhiteSalahSync: '#f7fff6',
+//         TertiarySalahSync: '#171717',
+//         RedSalahSync: '#f46036',
+//         YellowSalahSync: '#F5DD90',
+//         OrangeSalahSync: '#F68E5F',
+//       },
+//     },
+//   },
+//   darkMode: 'class',
+//   plugins: [nextui()],
+// };
+// export default config;
 import type { Config } from 'tailwindcss';
 const { nextui } = require('@nextui-org/react');
+const defaultTheme = require('tailwindcss/defaultTheme');
+const colors = require('tailwindcss/colors');
+const {
+  default: flattenColorPalette,
+} = require('tailwindcss/lib/util/flattenColorPalette');
 
-const config: Config = {
+/** @type {import('tailwindcss').Config} */
+module.exports = {
   content: [
     './pages/**/*.{js,ts,jsx,tsx,mdx}',
     './components/**/*.{js,ts,jsx,tsx,mdx}',
     './app/**/*.{js,ts,jsx,tsx,mdx}',
     './node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}',
   ],
+  darkMode: 'class',
   theme: {
     extend: {
       colors: {
@@ -15,15 +50,28 @@ const config: Config = {
         SecondarySalahSync: '#8CD867',
         LightSalahSync: '#EBF2FA',
         WhiteSalahSync: '#f7fff6',
-        DarkPrimarySalahSync: '#3C153B',
-        BlackSalahSync: '#171717',
+        TertiarySalahSync: '#171717',
         RedSalahSync: '#f46036',
         YellowSalahSync: '#F5DD90',
         OrangeSalahSync: '#F68E5F',
       },
     },
   },
-  darkMode: 'class',
-  plugins: [nextui()],
+  plugins: [
+    // rest of the code
+    [nextui()],
+    addVariablesForColors,
+  ],
 };
-export default config;
+
+// This plugin adds each Tailwind color as a global CSS variable, e.g. var(--gray-200).
+function addVariablesForColors({ addBase, theme }: any) {
+  let allColors = flattenColorPalette(theme('colors'));
+  let newVars = Object.fromEntries(
+    Object.entries(allColors).map(([key, val]) => [`--${key}`, val])
+  );
+
+  addBase({
+    ':root': newVars,
+  });
+}

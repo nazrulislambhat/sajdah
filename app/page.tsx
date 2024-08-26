d9e8ae6ed6940726fb9047181aa3f8f334802af3
Nazrul Islam <nazrulislambhat@gmail.com>
2024-08-26 12:17:37 +0530

    Tracker

--- app/page.tsx
+++ app/page.tsx
@@ -6,11 +6,14 @@ import Link from 'next/link';
 import Hero from '@/components/hero';
 import TasbeehCounter from '@/components/tasbeeh';
 import Duas from '@/components/duasCards';
-import PrayerTracker from '@/components/PrayerTracker';
+
 export default function Home() {
   return (
     <main className="min-h-screen boxed bg-LightSalahSync flex flex-col gap-4 xl:gap-8 items-center ">
-      <PrayerTracker />
+      <TasbeehCounter />
+      <Link href="/tracker" className="text-blue-700 underline">
+        Prayer Tracker (v0.1 alpha)
+      </Link>
     </main>
   );
 }
--- app/tracker/page.tsx
+++ app/tracker/page.tsx
@@ -1,3 +1,4 @@
+'use client';
 import { useState, useEffect } from 'react';
 import { CalendarIcon } from 'lucide-react';
 import { format } from 'date-fns';

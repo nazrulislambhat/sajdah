20653cfc15c8e43b8c8e29c5710afaafb16c605e
Nazrul Islam <nazrulislambhat@gmail.com>
2024-08-26 11:58:15 +0530

    Tracker

--- app/page.tsx
+++ app/page.tsx
@@ -6,11 +6,11 @@ import Link from 'next/link';
 import Hero from '@/components/hero';
 import TasbeehCounter from '@/components/tasbeeh';
 import Duas from '@/components/duasCards';
+import PrayerTracker from '@/components/PrayerTracker';
 export default function Home() {
   return (
     <main className="min-h-screen boxed bg-LightSalahSync flex flex-col gap-4 xl:gap-8 items-center ">
-      <TasbeehCounter />
-      <Duas />
+      <PrayerTracker />
     </main>
   );
 }

4c1752ac2f3a009d911c8820bb5f00088a561baa
Nazrul Islam <nazrulislambhat@gmail.com>
2024-08-27 11:15:19 +0530

    Tracker

---
+++ config/firebaseConfig.ts
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+  apiKey: 'AIzaSyAErADCm3oV3mZft9DlLo69H1kbwUXxuYc',
+  authDomain: 'prayertracker-1e48e.firebaseapp.com',
+  projectId: 'prayertracker-1e48e',
+  storageBucket: 'prayertracker-1e48e.appspot.com',
+  messagingSenderId: '1085385987618',
+  appId: '1:1085385987618:web:5b51af94a37e6e1bcb1c7b',
+  measurementId: 'G-V087D89QKG',
+};
